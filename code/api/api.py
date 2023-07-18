4e32a27a9f8192bb96e8bc0d8dc28f310014ecbd
JackEagles2 <jack.eagles@hotmail.co.uk>
2023-07-18 22:28:08 +0100

    Added a scheduler which will update the api variable

--- code/api/api.py
+++ code/api/api.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from flask import Flask, jsonify, request
-
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
 from ..webscraper import campus_leagues as cl
 
 # Can add leagues here
@@ -13,12 +14,29 @@ compsoc = {
         "https://sportsheffield.sportpad.net/leagues/view/1432/84", "CompSoc Greens")
 }
 app = Flask(__name__)
+scheduler = BackgroundScheduler(daemon=True)
+scheduler.start()
 
 
 # curl "http://127.0.0.1:5000/api/table?league=6s_Mon" | jq to display in terminal
 # curl "http://127.0.0.1:5000/api/table?league=6s_Wed" | jq to display in terminal
 # curl "http://127.0.0.1:5000/api/table?league=11s" | jq to display in terminal
 # Example endpoint that returns a JSON response
+def update_League():
+    global compsoc
+    compsoc = {
+        "6s_Mon": cl.LeagueTable(
+            "https://sportsheffield.sportpad.net/leagues/view/1426/84", "CompSoc Greens"),
+        "11s": cl.LeagueTable(
+            "https://sportsheffield.sportpad.net/leagues/view/1450/84", "Compsoc Greens"),
+        "6s_Wed": cl.LeagueTable(
+            "https://sportsheffield.sportpad.net/leagues/view/1432/84", "CompSoc Greens")
+    }
+
+
+scheduler.add_job(update_League, trigger=IntervalTrigger(days=5))  # Will update the league
+
+
 @app.route("/api/table", methods=["GET"])
 def get_table():
     league = request.args.get('league')
